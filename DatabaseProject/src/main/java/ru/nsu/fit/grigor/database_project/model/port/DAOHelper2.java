421ff834c964d6420ca176d810b6eee94e41debf
GrigorAmBc <grigor.abg70@gmail.com>
2020-05-24 09:08:41 +0700

    Created DAO layer. Waiting for implementation

---
+++ DatabaseProject/src/main/java/ru/nsu/fit/grigor/database_project/model/port/DAOHelper2.java
@@ -0,0 +1,4 @@
+package ru.nsu.fit.grigor.database_project.model.port;
+
+public interface DAOHelper2 {
+}
---
+++ DatabaseProject/src/main/java/ru/nsu/fit/grigor/database_project/model/port/ProductDAO.java
@@ -0,0 +1,8 @@
+package ru.nsu.fit.grigor.database_project.model.port;
+
+import javafx.util.Pair;
+
+public interface ProductDAO {
+
+  void get
+}
