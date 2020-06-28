aba9fe1eace2856c76eb5539335225c9c1d9c436
GrigorAmBc <grigor.abg70@gmail.com>
2020-06-28 18:16:59 +0700

    added database creation script

--- DatabaseProject/db.sql
+++ DatabaseProject/db.sql
@@ -16,6 +16,28 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+--
+-- Name: shop; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE shop WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';
+
+
+ALTER DATABASE shop OWNER TO postgres;
+
+\connect shop
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
@@ -322,4 +344,3 @@ ALTER TABLE ONLY public."SaleDetail"
 --
 -- PostgreSQL database dump complete
 --
-
